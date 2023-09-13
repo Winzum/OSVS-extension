06c2c9206239eba918ef184641abba24b22d1c04
Winzum <winzum93@gmail.com>
2023-09-13 10:46:55 +0200

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OSVS-extension
+
+Extension to check the viewstate in an OutSystems Traditional Web App.
+
+Currently only available as a debug extension.
+
+[Dot icons created by prettycons - Flaticon](https://www.flaticon.com/free-icons/dot)
---
+++ popup.js
@@ -0,0 +1,27 @@
+const byteSize = (str) => new Blob([str]).size;
+const browserAPI = window.browser ? browser : chrome;
+// chrome.tabs.executeScript(
+//   {
+//     code: 'document.querySelector("#__OSVSTATE").value',
+//   },
+//   function (results) {
+//     document.getElementById("placeholder").value = results[0];
+//   }
+// );
+
+let [tab] = await browserAPI.tabs.query({ active: true, currentWindow: true });
+
+let results = await browserAPI.scripting.executeScript({
+  target: { tabId: tab.id },
+  func: () => {
+    return document.querySelector("#__OSVSTATE").value;
+  },
+});
+
+results.then(
+  results[0].result
+    ? (document.querySelector("#placeholder").textContent =
+        byteSize(results[0].result) / 1000 + "kB")
+    : (document.querySelector("#placeholder").textContent =
+        "OS Viewstate not found")
+);
